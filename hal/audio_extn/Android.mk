134e80ae35c5f1f5d610db8e0d9c2938f3ef79e0
Jiyong Park <jiyong@google.com>
2021-08-30 15:29:07 +0900

    audio: Remove ndk_platform backend. Use the ndk backend.
    The ndk_platform backend will soon be deprecated because the ndk backend
    can serve the same purpose. This is to eliminate the confusion about
    having two variants (ndk and ndk_platform) for the same ndk backend.
    
    Bug: 161456198
    Test: m
    Merged-In: I14a1c57bd06f1f2aa52491f779c7030d4de03547
    Change-Id: I14a1c57bd06f1f2aa52491f779c7030d4de03547
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -1253,7 +1253,7 @@ LOCAL_C_INCLUDES:= \
         system/media/audio/include
 
 LOCAL_SHARED_LIBRARIES:= \
-        android.frameworks.automotive.powerpolicy-V1-ndk_platform \
+        android.frameworks.automotive.powerpolicy-V1-ndk \
         libbase \
         libbinder_ndk \
         libcutils \
